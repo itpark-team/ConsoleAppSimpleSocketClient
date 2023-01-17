7b876fdf88f7e6e17dcd9564e7fed358452b190f
itpark <a.m.smyslov@yandex.ru>
2023-01-17 20:45:35 +0300

    send client so class

--- ConsoleAppSimpleSocketClient/Program.cs
+++ ConsoleAppSimpleSocketClient/Program.cs
@@ -1,40 +1,12 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using ConsoleAppSimpleSocketClient;
 
-void Log(string msg)
-{
-    Console.WriteLine($"LOG: {DateTime.Now} --- {msg}");
-}
-
-Socket handler = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 34536);
-
-handler.Connect(ipEndPoint);
-
-Log($"CONNECTED TO SERVER {handler.RemoteEndPoint}");
+ClientEngine clientEngine = new ClientEngine("127.0.0.1", 34536);
+clientEngine.ConnectToServer();
 
 Console.Write("Введите сообщение для сервера: ");
 string messageToServer = Console.ReadLine();
 
-byte[] outputBytes = Encoding.Unicode.GetBytes(messageToServer);
-handler.Send(outputBytes);
+clientEngine.SendMessage(messageToServer);
+string messageFromServer = clientEngine.ReceiveMessage();
 
-Log($"MESSAGE TO SERVER SENT: {messageToServer}");
-
-StringBuilder messageBuilder = new StringBuilder();
-do
-{
-    byte[] inputBytes = new byte[1024];
-    int countBytes = handler.Receive(inputBytes);
-    messageBuilder.Append(Encoding.Unicode.GetString(inputBytes, 0, countBytes));
-} while (handler.Available > 0);
-
-string messageFromServer = messageBuilder.ToString();
-
-Log($"MESSAGE FROM SERVER RECIEVED: {messageFromServer}");
-
-handler.Shutdown(SocketShutdown.Both);
-handler.Close();
-
-Log($"CLIENT CLOSED");
+clientEngine.CloseClientSocket();
